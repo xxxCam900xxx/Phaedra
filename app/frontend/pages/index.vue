a795b2e238fc3d10602dffb74e43efcaf2da26f4
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-01 17:00:29 +0200

    ✨ Frontend: First Step of Adding Launcher

--- app/frontend/pages/index.vue
+++ app/frontend/pages/index.vue
@@ -1,3 +1,37 @@
+<script setup>
+useSeoMeta({
+    title: 'MythosMorph - Wizard',
+    ogTitle: 'MythosMorph - Wizard',
+})
+</script>
+
 <template>
-    <h1 class="text-4xl font-bold">Willkommen bei MythosMorph!</h1>
+    <main class="h-screen flex justify-center items-center">
+
+        <div class="max-w-[1000px] flex flex-col gap-2 rounded-md bg-sky-200 p-10 backdrop-blur-sm shadow-lg">
+            <h1 class="text-5xl font-bold">MythosMorph Laucher</h1>
+            <p>Willkommen bei MythosMorph! Dein eigener Webseiten Editor. Gehe mit mir den Wizard durch und danach
+                kannst du in Ruhe deine Webseite gestalten!</p>
+
+            <form action="">
+                <!-- Launcher Step1 -->
+                <!-- Wie heisst deine Webseite? -->
+                <!-- Wenn du ein Logo hast, lade es hier hoch! -->
+
+                <!-- Launcher Step2 -->
+                <!-- Wie heisst deine Webseite? -->
+                <!-- Wenn du ein Logo hast, lade es hier hoch! -->
+
+                <!-- Launcher Step3 -->
+                <!-- Wer betreibt diese Webseite? -->
+                <!-- Wie kann man dich am besten erreichen? -->
+
+                <!-- Launcher Step4 -->
+                <!-- Definiere ein Passwort -->
+
+                <!-- Launcher Completed -->
+                <!-- Weiterleitung zum Login Form -->
+            </form>
+        </div>
+    </main>
 </template>
