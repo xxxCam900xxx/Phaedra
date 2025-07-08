12efc74fc442b653a5fba8f8874ed747dacc3273
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-08 15:01:23 +0200

    ✨ App: imporved navigation look

--- app/assets/components/navigation/dynamicNavigation.php
+++ app/assets/components/navigation/dynamicNavigation.php
@@ -4,9 +4,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/api/editor/pages/getAllPages.php';
 $pages = getAllPages();
 ?>
 
-<header class="h-[60px] w-full bg-sky-300 flex gap-2">
-    <div class="logo h-full"></div>
-    <nav>
+<header class="h-[60px] w-full bg-sky-300 flex gap-5 pl-[10%] pr-[10%]">
+    <a href="/" class="logo h-full bg-black w-[60px]"></a>
+    <nav class="flex gap-2 items-center">
         <?php
 
         if ($pages === false) {
@@ -14,9 +14,14 @@ $pages = getAllPages();
         } else {
             foreach ($pages as $page) {
 
-                ?>
-                <a href="<?= $page['pathURL'] ?>"><?= $page['title'] ?></a>
-                <?php
+        ?>
+                <a
+                    href="<?= $page['pathURL'] ?>"
+                    class="p-2 text-lg rounded-md hover:text-white hover:bg-sky-500 trainsition duration-300"
+                    >
+                    <?= $page['title'] ?>
+                </a>
+        <?php
             }
         }
 
