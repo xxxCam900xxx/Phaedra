0f45c0390d22e118e0015afc96844228872d7644
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-08 15:07:28 +0200

    🐛 App: fixed missing data for Layout/Widget on TwoSplitLayout

--- app/assets/components/layouts/TwoSplitLayout.php
+++ app/assets/components/layouts/TwoSplitLayout.php
@@ -1,59 +1,37 @@
-<?php
+<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/api/editor/widgets/getWidget.php"; ?>
 
-/** @var array $data enthält z.B.: [ 'no1_widget_id' => 42 ] */
-?>
-<?php require_once $_SERVER["DOCUMENT_ROOT"] . "/api/editor/widgets/getWidget.php" ?>
+<div class="Layout w-full min-h-[100px] flex gap-5" 
+     data-layout-id="<?= htmlspecialchars($layoutID) ?>" 
+     data-layout-type="<?= htmlspecialchars($type) ?>">
 
-<div class="Layout w-full min-h-[100px] flex gap-5" data-layout-id="<?= htmlspecialchars($layoutID) ?>"
-    data-layout-type="<?= htmlspecialchars($type) ?>">
-    <div class="Widget w-full min-h-[100px]" data-Widget-Slot="1">
-        <?php
-        if (!empty($data['no1_widget_id']) && !empty($data['no1_widget_type'])) {
-            $widgetId = (int) $data['no1_widget_id'];
-            $widgetType = $data['no1_widget_type'];
+    <?php for ($i = 1; $i <= 2; $i++): 
+        $widgetIdKey = "no{$i}_widget_id";
+        $widgetTypeKey = "no{$i}_widget_type";
 
-            // Widgetdaten laden
-            $widgetData = getWidgetData($widgetType, $widgetId);
+        $hasWidget = !empty($data[$widgetIdKey]) && !empty($data[$widgetTypeKey]);
+        $widgetId = $hasWidget ? (int)$data[$widgetIdKey] : '';
+        $widgetType = $hasWidget ? htmlspecialchars($data[$widgetTypeKey]) : '';
+    ?>
+        <div class="Widget w-full min-h-[100px]" 
+             data-widget-slot="<?= $i ?>" 
+             <?= $hasWidget ? "data-widget-id=\"$widgetId\" data-widget-type=\"$widgetType\"" : "" ?>>
+             
+            <?php if ($hasWidget): 
+                $widgetData = getWidgetData($widgetType, $widgetId);
+                if ($widgetData):
+                    $widgetFile = $_SERVER['DOCUMENT_ROOT'] . "/assets/components/widgets/{$widgetType}.php";
+                    if (file_exists($widgetFile)):
+                        include $widgetFile;
+                    else: ?>
+                        <div class="text-red-500">
+                            Widget-Komponente nicht gefunden: <?= htmlspecialchars($widgetType) ?>
+                        </div>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <div>Widget-Daten nicht gefunden</div>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    <?php endfor; ?>
 
-            if ($widgetData) {
-                // Widget-Komponenten-Datei (Pfad anpassen)
-                $widgetFile = $_SERVER['DOCUMENT_ROOT'] . "/assets/components/widgets/{$widgetType}.php";
-
-                if (file_exists($widgetFile)) {
-                    include $widgetFile; // Diese Datei nutzt $widgetData zum Rendern
-                } else {
-                    echo "<div class='text-red-500'>Widget-Komponente nicht gefunden: " . htmlspecialchars($widgetType) . "</div>";
-                }
-            } else {
-                echo "<div>Widget-Daten nicht gefunden</div>";
-            }
-        } else {
-        }
-        ?>
-    </div>
-    <div class="Widget w-full min-h-[100px]" data-Widget-Slot="2">
-        <?php
-        if (!empty($data['no2_widget_id']) && !empty($data['no2_widget_type'])) {
-            $widgetId = (int) $data['no2_widget_id'];
-            $widgetType = $data['no2_widget_type'];
-
-            // Widgetdaten laden
-            $widgetData = getWidgetData($widgetType, $widgetId);
-
-            if ($widgetData) {
-                // Widget-Komponenten-Datei (Pfad anpassen)
-                $widgetFile = $_SERVER['DOCUMENT_ROOT'] . "/assets/components/widgets/{$widgetType}.php";
-
-                if (file_exists($widgetFile)) {
-                    include $widgetFile; // Diese Datei nutzt $widgetData zum Rendern
-                } else {
-                    echo "<div class='text-red-500'>Widget-Komponente nicht gefunden: " . htmlspecialchars($widgetType) . "</div>";
-                }
-            } else {
-                echo "<div>Widget-Daten nicht gefunden</div>";
-            }
-        } else {
-        }
-        ?>
-    </div>
 </div>
