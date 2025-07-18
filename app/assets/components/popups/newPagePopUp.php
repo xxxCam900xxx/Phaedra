038c176088e147f04682c24bf6fd3da7878de147
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-18 15:51:54 +0200

    ✨ App: added Editor PopUp Design for Sites

--- app/assets/components/popups/newPagePopUp.php
+++ app/assets/components/popups/newPagePopUp.php
@@ -1,21 +1,48 @@
 <!-- Create New Page PopUp -->
-<div id="newPageModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
-    <div class="bg-white rounded-lg p-6 w-96">
-        <h2 class="text-xl mb-4">Neue Seite erstellen</h2>
-        <form id="newPageForm" class="flex flex-col gap-3">
+<div id="newPageModal" class="fixed inset-0 phaedra-popup-background flex items-center justify-center hidden">
+    <div class="bg-white rounded-lg w-96 relative shadow-xl">
+
+        <!-- Close Button -->
+        <button onclick="javascript:newPageModal.classList.add('hidden');" class="absolute shadow-xl cursor-pointer text-xl top-[-15px] right-[-15px] w-[40px] h-[40px] phaedra-scondary-backgroundcolor z-index-10 rounded-md">
+            <i class="fa-solid fa-x"></i>
+        </button>
+
+        <!-- HeadTitle -->
+        <div class="phaedra-scondary-backgroundcolor p-5 rounded-t-md">
+            <h2 class="text-2xl font-semibold">Seite bearbeiten / erstellen</h2>
+        </div>
+
+        <form id="newPageForm" class="flex flex-col gap-3 p-5">
+
             <input type="text" name="id" class="hidden" />
-            <input type="text" name="nav_title" placeholder="Navigations Titel" required class="border p-2 rounded" />
-            <input type="text" name="pathURL" placeholder="URL Pfad" required class="border p-2 rounded" />
-            <input type="text" name="page_title" placeholder="Seiten Titel" class="border p-2 rounded" />
-            <textarea name="meta_description" placeholder="Meta-Beschreibung" rows="3"
-                class="border p-2 rounded"></textarea>
-            <input type="number" name="sort" placeholder="Sortierung" class="border p-2 rounded" />
+
+            <div class="flex flex-col">
+                <label for="nav_title">Navigation Titel</label>
+                <input type="text" name="nav_title" placeholder="Navigations Titel" required class="border p-2 rounded" />
+            </div>
+
+            <div class="flex flex-col">
+                <label for="nav_title">Url</label>
+                <input type="text" name="pathURL" placeholder="URL Pfad" required class="border p-2 rounded" />
+            </div>
+            <div class="flex flex-col">
+                <label for="nav_title">Seitentitel</label>
+                <input type="text" name="page_title" placeholder="Seiten Titel" class="border p-2 rounded" />
+            </div>
+            <div class="flex flex-col">
+                <label for="nav_title">Meta-Beschreibung</label>
+                <textarea name="meta_description" placeholder="Meta-Beschreibung" rows="3"
+                    class="border p-2 rounded">
+                </textarea>
+            </div>
+            <div class="flex flex-col">
+                <label for="sort">Sortierung</label>
+                <input type="number" name="sort" placeholder="Sortierung" class="border p-2 rounded" />
+            </div>
 
             <div class="flex justify-end gap-2 mt-4">
-                <button type="button" id="cancelBtn"
-                    class="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">Abbrechen</button>
-                <button type="submit"
-                    class="px-4 py-2 bg-sky-500 text-white rounded hover:bg-sky-600">Erstellen</button>
+                <button type="button" id="cancelBtn" class="px-4 py-2 phaedra-scondary-backgroundcolor rounded-md">Abbrechen</button>
+                <button type="submit" class="px-4 py-2 text-white rounded bg-emerald-500 hover:bg-emerald-600">Erstellen</button>
             </div>
         </form>
     </div>
--- app/assets/components/popups/showPageStructurePopUp.php
+++ app/assets/components/popups/showPageStructurePopUp.php
@@ -1,8 +1,19 @@
 <!-- Page Structure PopUp -->
-<div id="showPageStructure" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center hidden">
-    <div class="bg-white rounded-lg p-6 w-96 relative">
-        <h2 class="text-xl mb-4">Seitenstruktur</h2>
-        <div class="flex flex-col gap-2" id="pageListContainer">
+<div id="showPageStructure" class="fixed inset-0 phaedra-popup-background flex items-center justify-center hidden">
+
+    <div class="bg-white rounded-lg w-96 relative shadow-xl">
+
+        <!-- Close Button -->
+        <button id="showPageStructureCancelBtn" class="absolute shadow-xl cursor-pointer text-xl top-[-15px] right-[-15px] w-[40px] h-[40px] phaedra-scondary-backgroundcolor z-index-10 rounded-md">
+            <i class="fa-solid fa-x"></i>
+        </button>
+
+        <!-- HeadTitle -->
+        <div class="phaedra-scondary-backgroundcolor p-5 rounded-t-md">
+            <h2 class="text-2xl font-semibold">Seitenstruktur</h2>
+        </div>
+
+        <div class="flex flex-col p-5 gap-2 overflow-auto max-h-[500px]" id="pageListContainer">
             <?php
             require_once $_SERVER['DOCUMENT_ROOT'] . '/api/editor/pages/getAllPages.php';
             $pages = getAllPages();
@@ -13,22 +24,22 @@
                 foreach ($pages as $page) {
 
             ?>
-                    <div class="p-2 text-lg flex justify-between hover:bg-gray-200 rounded-md trainsition duration-300" data-id="<?= $page['id'] ?>">
-                        <?= $page['nav_title'] ?>
-                        <div class="flex gap-2">
-                            <p class="min-w-[50px] text-center rounded-md bg-sky-300"><?= $page['sort'] ?></p>
-                            <!-- Update -->
-                            <button class="cursor-pointer flex items-center justify-center w-[30px] h-[30px] hover:text-white hover:bg-sky-500 rounded-md trainsition duration-300" onclick="openUpdatePagePopUp('<?= $page['id'] ?>', '<?= $page['nav_title'] ?>', '<?= $page['pathURL'] ?>', '<?= $page['page_title'] ?>', '<?= $page['meta_description'] ?>', '<?= $page['sort'] ?>')"><i class="fa-solid fa-file-pen"></i></button>
-                            <!-- Delete -->
-                            <button class="cursor-pointer flex items-center justify-center w-[30px] h-[30px] hover:text-white hover:bg-red-500 rounded-md trainsition duration-300" onclick="deletePageById(<?= $page['id'] ?>)"><i class="fa-solid fa-trash"></i></button>
+                    <div class="flex gap-2 w-fulll items-center" data-id="<?= $page['id'] ?>">
+                        <div class="p-2 w-full text-lg flex justify-between phaedra-scondary-backgroundcolor rounded-md trainsition duration-300">
+                            <?= $page['nav_title'] ?>
+                            <div class="flex gap-2">
+                                <p class="min-w-[50px] text-center rounded-md bg-white"><?= $page['sort'] ?></p>
+                            </div>
                         </div>
+                        <!-- Update -->
+                        <button class="cursor-pointer text-xl phaedra-scondary-backgroundcolor flex items-center justify-center h-[44px] aspect-square hover:text-white hover:bg-sky-500 rounded-md trainsition duration-300" onclick="openUpdatePagePopUp('<?= $page['id'] ?>', '<?= $page['nav_title'] ?>', '<?= $page['pathURL'] ?>', '<?= $page['page_title'] ?>', '<?= $page['meta_description'] ?>', '<?= $page['sort'] ?>')"><i class="fa-solid fa-file-pen"></i></button>
+                        <!-- Delete -->
+                        <button class="cursor-pointer text-xl phaedra-scondary-backgroundcolor flex items-center justify-center h-[44px] aspect-square hover:text-white hover:bg-red-500 rounded-md trainsition duration-300" onclick="deletePageById(<?= $page['id'] ?>)"><i class="fa-solid fa-trash"></i></button>
                     </div>
             <?php
                 }
             }
             ?>
         </div>
-        <button type="button" id="showPageStructureCancelBtn"
-            class="px-4 absolute top-5 right-5 py-2 bg-gray-300 rounded hover:bg-gray-400 cursor-pointer"><i class="fa-solid fa-xmark"></i></button>
     </div>
 </div>
