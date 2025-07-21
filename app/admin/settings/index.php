ec862deab9c0a9f353d3007fe063881212e725b1
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-21 09:12:18 +0200

    ✨ App: added Design for Settings

--- app/admin/settings/index.php
+++ app/admin/settings/index.php
@@ -21,68 +21,56 @@ $webConfig = getWebConfig();
         <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/assets/components/navigation/adminNavigation.php" ?>
         <div class="platzhalter w-[100px] h-screen"></div>
 
-        <section class="p-5 flex w-full gap-5">
+        <section class="p-5 flex w-full items-center justify-center gap-5">
 
             <!-- Konfigurationsformular -->
-            <form id="webConfigForm" class="flex flex-col w-full gap-5" method="POST" action="/api/webconfig/saveWebConfig.php" enctype="multipart/form-data">
+            <form id="webConfigForm" class="flex p-5 flex-col bg-white shadow-lg rounded-xl min-w-[500px] gap-5" method="POST" action="/api/webconfig/saveWebConfig.php" enctype="multipart/form-data">
 
-                <div class="flex gap-5 w-full">
-                    <div class="w-1/2 flex flex-col gap-5">
-                        <div class="w-full bg-sky-100 flex flex-col rounded-xl overflow-hidden">
-                            <label class="w-full bg-sky-700 text-white p-3 text-2xl" for="webName">Webseiten Name</label>
-                            <div class="p-5">
-                                <input class="p-2 w-full bg-white" type="text" name="webName" placeholder="example.com" value="<?= htmlspecialchars($webConfig->WebsiteName) ?>">
-                            </div>
+                <div class="">
+                    <label class="text-2xl phaedra-primary-color font-semibold" for="webName">Domain Name</label>
+                    <input class="p-2 w-full rounded-md border border-blue-800" type="text" name="webName" placeholder="example.com" value="<?= htmlspecialchars($webConfig->WebsiteName) ?>">
+                </div>
+
+                <div class="">
+                    <label class="text-2xl phaedra-primary-color font-semibold" for="webHostName">Hostname</label>
+                    <input class="p-2 w-full rounded-md border border-blue-800" type="text" name="webHostName" placeholder="support@email.com" value="<?= htmlspecialchars($webConfig->WebHostName) ?>">
+                </div>
+
+                <div class="">
+                    <label class="text-2xl phaedra-primary-color font-semibold" for="webContact">Email Adresse</label>
+                    <input class="p-2 w-full rounded-md border border-blue-800" type="email" name="webContact" placeholder="example@sample.com" value="<?= htmlspecialchars($webConfig->WebContact) ?>">
+                </div>
+
+                <!-- Hidden Input für Logo-Pfad -->
+                <input type="hidden" id="webLogoURL" name="webLogoURL" value="<?= htmlspecialchars($webConfig->WebLogoURL) ?>" />
+                <div>
+                    <label class="text-2xl phaedra-primary-color font-semibold">Webseiten Logo</label>
+                    <div class="flex gap-5 p-5 rounded-md border border-blue-800">
+                        <!-- Vorschau -->
+                        <div id="logoPreview" class="w-1/2">
+                            <?php if (!empty($webConfig->WebLogoURL)): ?>
+                                <img src="<?= htmlspecialchars($webConfig->WebLogoURL) ?>" alt="Aktuelles Logo" class="max-h-[200px] mx-auto" />
+                            <?php else: ?>
+                                <img src="" alt="Kein Logo ausgewählt" class="max-h-[200px] mx-auto" />
+                            <?php endif; ?>
                         </div>
 
-                        <div class="w-full bg-sky-100 flex flex-col rounded-xl overflow-hidden">
-                            <label class="w-full bg-sky-700 text-white p-3 text-2xl" for="webHostName">Webseiten Hostname</label>
-                            <div class="p-5">
-                                <input class="p-2 w-full bg-white" type="text" name="webHostName" placeholder="support@email.com" value="<?= htmlspecialchars($webConfig->WebHostName) ?>">
-                            </div>
-                        </div>
-
-                        <div class="w-full bg-sky-100 flex flex-col rounded-xl overflow-hidden">
-                            <label class="w-full bg-sky-700 text-white p-3 text-2xl" for="webContact">Webseiten Email</label>
-                            <div class="p-5">
-                                <input class="p-2 w-full bg-white" type="email" name="webContact" placeholder="example@sample.com" value="<?= htmlspecialchars($webConfig->WebContact) ?>">
-                            </div>
+                        <!-- Drag & Drop Bereich -->
+                        <div id="imageDropArea" class="w-1/2 flex items-center justify-center border-4 border-dashed border-gray-400 rounded-lg p-10 text-center text-gray-600 cursor-pointer hover:border-blue-800 transition">
+                            Ziehen Sie hier Ihr Logo-Bild hinein oder klicken Sie, um auszuwählen.
+                            <input type="file" id="imageInput" accept="image/*" class="hidden" />
                         </div>
                     </div>
 
 
-                    <!-- Hidden Input für Logo-Pfad -->
-                    <input type="hidden" id="webLogoURL" name="webLogoURL" value="<?= htmlspecialchars($webConfig->WebLogoURL) ?>" />
-
-                    <div class="w-1/2 bg-sky-100 flex flex-col items-center rounded-xl overflow-hidden">
-                        <label class="w-full bg-sky-700 text-white p-3 text-2xl mb-3">Website Logo Upload / Auswahl</label>
-
-                        <div class="flex gap-5 p-5">
-                            <!-- Vorschau -->
-                            <div id="logoPreview" class="mt-4 w-1/2">
-                                <?php if (!empty($webConfig->WebLogoURL)): ?>
-                                    <img src="<?= htmlspecialchars($webConfig->WebLogoURL) ?>" alt="Aktuelles Logo" class="max-h-[200px] mx-auto" />
-                                <?php else: ?>
-                                    <img src="" alt="Kein Logo ausgewählt" class="max-h-[200px] mx-auto" />
-                                <?php endif; ?>
-                            </div>
-
-                            <!-- Drag & Drop Bereich -->
-                            <div id="imageDropArea" class="w-1/2 flex items-center justify-center border-4 border-dashed border-gray-400 rounded-lg p-10 text-center text-gray-600 cursor-pointer hover:border-sky-500 transition">
-                                Ziehen Sie hier Ihr Logo-Bild hinein oder klicken Sie, um auszuwählen.
-                                <input type="file" id="imageInput" accept="image/*" class="hidden" />
-                            </div>
-                        </div>
-
-                        <!-- Button für Popup -->
-                        <button id="openImageSelectorBtn" type="button" class="w-fit mt-5 bg-sky-600 hover:bg-sky-700 text-white px-4 py-2 rounded-md">
-                            Logo aus Bibliothek auswählen
-                        </button>
-                    </div>
+                    <!-- Button für Popup -->
+                    <button id="openImageSelectorBtn" type="button" class="w-fit mt-2 font-semibold phaedra-scondary-backgroundcolor hover:bg-sky-700 text-white px-4 py-2 rounded-md">
+                        Logo aus Bibliothek auswählen
+                    </button>
                 </div>
 
                 <div class="flex justify-end mt-6">
-                    <button id="webConfigFormSaveBtn" type="submit" class="bg-green-400 cursor-pointer text-white rounded-md p-2 text-xl">
+                    <button id="webConfigFormSaveBtn" type="submit" class="bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4 rounded w-fit">
                         Speichern
                     </button>
                 </div>
