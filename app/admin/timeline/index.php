1f5f2b0486dc0b44da681f5f3c3be589aa5449e5
Developer <112639963+xxxCam900xxx@users.noreply.github.com>
2025-07-22 15:47:04 +0200

    ✨ App: added Design for Timeline

--- app/admin/timeline/index.php
+++ app/admin/timeline/index.php
@@ -21,67 +21,70 @@ $timeLines = getTimeLines();
         <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/assets/components/navigation/adminNavigation.php" ?>
         <div class="platzhalter w-[100px] h-screen"></div>
 
-        <section class="flex flex-col lg:flex-row gap-8 p-8">
+        <section class="flex flex-col w-full lg:flex-row gap-8 p-8">
             <!-- TimeLine Tabellen Liste -->
-            <div class="w-full lg:w-2/3 flex flex-col gap-5">
-                <h2 class="text-2xl font-semibold">TimeLine Übersicht</h2>
-                <table class="min-w-full bg-white border border-gray-300 rounded-lg overflow-hidden shadow-sm">
-                    <thead class="bg-gray-100 text-left text-sm font-medium text-gray-700">
-                        <tr>
-                            <th class="px-4 py-3 border-b">Datum</th>
-                            <th class="px-4 py-3 border-b">Titel</th>
-                            <th class="px-4 py-3 border-b">Beschreibung</th>
-                            <th class="px-4 py-3 border-b">Link</th>
-                            <th class="px-4 py-3 border-b"></th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-sm divide-y divide-gray-200">
-                        <?php foreach ($timeLines as $timeLine): 
-                            $dateWithoutTime = strtotime($timeLine['Date']); ?>
-                            <tr class="hover:bg-gray-50">
-                                <td class="px-4 py-3"><?= date('d.m.Y', $dateWithoutTime); ?></td>
-                                <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Title']) ?></td>
-                                <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Description']) ?></td>
-                                <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Link']) ?></td>
-                                <td class="px-4 py-3 flex gap-2 justify-end">
-                                    <button
-                                        class="text-blue-600 hover:underline text-sm"
-                                        onclick="editTimeLine(this)"
-                                        data-id="<?= $timeLine['ID'] ?>"
-                                        data-date="<?= htmlspecialchars(date('Y-m-d', $dateWithoutTime), ENT_QUOTES) ?>"
-                                        data-title="<?= htmlspecialchars($timeLine['Title'], ENT_QUOTES) ?>"
-                                        data-description="<?= htmlspecialchars($timeLine['Description'], ENT_QUOTES) ?>"
-                                        data-link="<?= htmlspecialchars($timeLine['Link'], ENT_QUOTES) ?>">
-                                        Bearbeiten
-                                    </button>
-                                    <button
-                                        class="text-blue-600 hover:underline text-sm"
-                                        onclick="deleteTimeLine(this)"
-                                        data-id="<?= $timeLine['ID'] ?>">
-                                        Löschen
-                                    </button>
-                                </td>
+            <div class="w-full lg:w-2/3 flex flex-col gap-5 bg-white overflow-hidden rounded-lg shadow-lg h-fit">
+                <h2 class="text-2xl phaedra-scondary-backgroundcolor font-semibold p-5">TimeLine Übersicht</h2>
+                <div class="px-5">
+                    <table class="min-w-full bg-white border border-gray-300 rounded-lg overflow-hidden shadow-sm">
+                        <thead class="phaedra-scondary-backgroundcolor text-left text-sm font-medium">
+                            <tr>
+                                <th class="px-4 py-3 border-b">Datum</th>
+                                <th class="px-4 py-3 border-b">Titel</th>
+                                <th class="px-4 py-3 border-b">Beschreibung</th>
+                                <th class="px-4 py-3 border-b">Link</th>
+                                <th class="px-4 py-3 border-b"></th>
                             </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-
-                <button
-                    class="text-white hover:bg-blue-500 cursor-pointer transition duration-300 p-3 rounded-md bg-sky-300"
-                    onclick="editTimeLine(this)"
-                    data-id=""
-                    data-date=""
-                    data-title=""
-                    data-description=""
-                    data-link="">
-                    Neue TimeLine erstellen
-                </button>
+                        </thead>
+                        <tbody class="text-sm divide-y divide-gray-200">
+                            <?php foreach ($timeLines as $timeLine):
+                                $dateWithoutTime = strtotime($timeLine['Date']); ?>
+                                <tr class="hover:bg-gray-50">
+                                    <td class="px-4 py-3"><?= date('d.m.Y', $dateWithoutTime); ?></td>
+                                    <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Title']) ?></td>
+                                    <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Description']) ?></td>
+                                    <td class="px-4 py-3"><?= htmlspecialchars($timeLine['Link']) ?></td>
+                                    <td class="px-4 py-3 flex gap-2 justify-end">
+                                        <button
+                                            class="text-blue-600 hover:underline text-sm"
+                                            onclick="editTimeLine(this)"
+                                            data-id="<?= $timeLine['ID'] ?>"
+                                            data-date="<?= htmlspecialchars(date('Y-m-d', $dateWithoutTime), ENT_QUOTES) ?>"
+                                            data-title="<?= htmlspecialchars($timeLine['Title'], ENT_QUOTES) ?>"
+                                            data-description="<?= htmlspecialchars($timeLine['Description'], ENT_QUOTES) ?>"
+                                            data-link="<?= htmlspecialchars($timeLine['Link'], ENT_QUOTES) ?>">
+                                            Bearbeiten
+                                        </button>
+                                        <button
+                                            class="text-blue-600 hover:underline text-sm"
+                                            onclick="deleteTimeLine(this)"
+                                            data-id="<?= $timeLine['ID'] ?>">
+                                            Löschen
+                                        </button>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="flex justify-end pb-5 px-5">
+                    <button
+                        class="phaedra-scondary-backgroundcolor w-fit text-white cursor-pointer transition duration-300 p-3 rounded-md"
+                        onclick="editTimeLine(this)"
+                        data-id=""
+                        data-date=""
+                        data-title=""
+                        data-description=""
+                        data-link="">
+                        Neue TimeLine erstellen
+                    </button>
+                </div>
             </div>
 
             <!-- Bearbeitungsformular -->
-            <div class="w-full lg:w-1/3">
-                <h2 class="text-2xl font-semibold mb-4">TimeLine bearbeiten</h2>
-                <form id="newTimeLineForm" class="bg-white shadow-sm rounded-lg p-6 space-y-5 border border-gray-200">
+            <div class="w-full lg:w-1/3 bg-white shadow-lg rounded-lg h-fit overflow-hidden">
+                <h2 class="text-2xl font-semibold mb-4 p-5 phaedra-scondary-backgroundcolor">TimeLine bearbeiten</h2>
+                <form id="newTimeLineForm" class="space-y-5 px-5 pb-5">
                     <input type="hidden" name="ID" id="ID">
 
                     <div>
@@ -108,8 +111,11 @@ $timeLines = getTimeLines();
                             class="w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-200" />
                     </div>
 
-                    <button type="submit"
-                        class="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition">Speichern</button>
+                    <div class="flex justify-end mt-6">
+                        <button id="webConfigFormSaveBtn" type="submit" class="bg-emerald-500 hover:bg-emerald-600 text-white py-2 px-4 rounded w-fit">
+                            Speichern
+                        </button>
+                    </div>
                 </form>
             </div>
         </section>
